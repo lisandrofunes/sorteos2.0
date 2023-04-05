882ff3998b4acba6c1e8d68c7e62ab6b33c08402
lisandrofunes <lisandrofunes05@gmail.com>
2023-04-04 22:45:47 -0300

    changed lottery name according to the type

--- src/app/sorteo/sorteo.component.ts
+++ src/app/sorteo/sorteo.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute, ActivatedRouteSnapshot } from '@angular/router';
+import { ActivatedRoute, ActivatedRouteSnapshot, NavigationEnd, Router } from '@angular/router';
 import { ParticipanteHandle } from '../models/participante';
 import { Sorteo } from '../models/sorteo';
 import { SorteoService } from '../Service/sorteo.service';
@@ -17,41 +17,88 @@ export class SorteoComponent {
   resultadoSorteo: string[] = [];
   participantes: ParticipanteHandle[] = [];
   isSorteado = false;
+  isHidden = true;
 
   constructor(
     private sorteoService: SorteoService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) { }
 
-
   ngOnInit(): void {
+    this.route.params.subscribe(
+      params=>{
+        this.changeNameSorteo(params['id'])
+      }
+    )
   }
+
+  changeNameSorteo(parametro:string){
+    // const parametro = this.route.snapshot.paramMap.get('id');
+
+    const titleSorteo = document.querySelector('.title-form');
+    switch (parametro) {
+      case '0':
+        titleSorteo!.innerHTML = "Sorteo Simple";
+      break;
+
+      case '1':
+        titleSorteo!.innerHTML = "Formar Parejas";
+      break;
+    
+      default:
+        titleSorteo!.innerHTML = "Sorteo Simple";
+      break;
+    }
+  }
+
 
   addParticipante() {
     this.inputCount++;
     this.inputArray.push('');
     this.valorInput.push('');
+    this.isHidden = false;
   }
 
   sortear() {
+    var pCount = this.inputCount; //cantidad de participantes
+    var rCount = 0; //iterador de posiciones para resultadoSorteo
     const parametro = this.route.snapshot.paramMap.get('id');
+
     switch (parametro) {
       case '1':
-        console.log("sorteo tipo 1")
+        this.isSorteado = true;
+
+        if(pCount%2==0){
+          console.log("sorteo posible")
+        } else{
+          console.log("advertencia")
+        }
+
+        for (let i = 0; i < pCount; i++) {
+          const numeroAleatorio = Math.floor(Math.random() * this.valorInput.length);
+          if(i%2==0){
+            this.resultadoSorteo.push(this.valorInput[numeroAleatorio]);
+          } else {
+            this.resultadoSorteo[rCount] += (' - ' + this.valorInput[numeroAleatorio].toString());
+            rCount++
+          }
+          this.valorInput.splice(numeroAleatorio, 1);
+        }
+
+        this.guardarSorteo()
 
         break;
 
       default:
         this.isSorteado = true;
 
-        const pCount = this.inputCount;
+        // const pCount = this.inputCount;
 
         for (let i = 0; i < pCount; i++) {
           const numeroAleatorio = Math.floor(Math.random() * this.valorInput.length);
-          console.log("aleatorio: " + numeroAleatorio);
           this.resultadoSorteo.push(this.valorInput[numeroAleatorio]);
           this.valorInput.splice(numeroAleatorio, 1);
-          console.log(this.valorInput.length + "rest")
         }
 
         this.guardarSorteo()
@@ -74,14 +121,9 @@ export class SorteoComponent {
   }
 
   prepareForData(): FormData {
-    // Obtener la fecha y hora actual
-    const fechaHoraActual = new Date();
-
-    // Obtener la fecha actual en formato ISO (por ejemplo: "2023-03-30")
-    const fechaActual = fechaHoraActual.toISOString().slice(0, 10);
-
-    // Obtener la hora actual en formato de 24 horas (por ejemplo: "14:30:00")
-    const horaActual24 = fechaHoraActual.toTimeString().slice(0, 8);
+    const fechaHoraActual = new Date();// Obtener la fecha y hora actual
+    const fechaActual = fechaHoraActual.toISOString().slice(0, 10);// Obtener la fecha actual en formato ISO (por ejemplo: "2023-03-30")
+    const horaActual24 = fechaHoraActual.toTimeString().slice(0, 8);// Obtener la hora actual en formato de 24 horas (por ejemplo: "14:30:00")
 
     const sorteo = {
       fecha: fechaActual,
@@ -104,5 +146,9 @@ export class SorteoComponent {
     this.inputCount--;
     this.inputArray.splice(index, 1);
     this.valorInput.splice(index, 1);
+    if(this.inputCount==1){
+      this.isHidden = true;
+    }
   }
+
 }
